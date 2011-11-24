b1c3e2431322779afc207247264f72ce474e029d
Fumie Toyooka <dymojet@google.com>
2011-11-24 17:02:27 +0900

    my first commit!

---
+++ apps/pc_frontend/modules/favicon/actions/actions.class.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * favicon actions.
+ *
+ * @package    OpenPNE
+ * @subpackage favicon
+ * @author     Your name here
+ */
+class faviconActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfWebRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+  }
+}
